11a173a184d45335e83f50c43a130b56acdd772b
Flo Wirtz <6052785+flowirtz@users.noreply.github.com>
2024-07-03 14:32:17 +0100

    🎉 Initial commit

---
+++ docs/quickstart.md
@@ -0,0 +1,104 @@
+---
+title: 'Quickstart'
+description: 'From Zero to Pipeline in Under 7 Minutes'
+sidebar_position: 1
+---
+
+This guide will help to quickly setup Dracon on a Kubernetes cluster and get a
+pipeline running. The first step is to create a dev Kubernetes cluster in order
+to deploy Tekton. We suggest you use KiND to provision a local test cluster
+quickly. If you already have a K8s cluster then you can skip directly to the
+[Deploying Dracon dependencies](#2-deploying-dracon-dependencies) section.
+
+## Requirements
+
+You will need to have the following tools installed in your system:
+
+1. [KiND](https://kind.sigs.k8s.io/docs/user/quick-start/#installation)
+2. [kustomize](https://kubectl.docs.kubernetes.io/installation/kustomize/)
+3. [Docker](https://docs.docker.com/engine/install/)
+4. [Helm](https://helm.sh/docs/intro/install/)
+
+
+## Installation
+### 1. Setting up a [KinD](https://kind.sigs.k8s.io/) cluster
+
+KinD is is a tool for running local Kubernetes clusters using Docker container
+“nodes”. You can create a cluster with the following command:
+
+```bash
+./scripts/kind-with-registry.sh
+```
+
+### 2. Deploying Dracon Dependencies
+
+Next, you will need to deploy a bunch of dependencies that Dracon can't work without. You can do this by running the following command:
+
+```bash
+make dev-deploy DRACON_VERSION=v0.15.0
+```
+
+This will deploy various components to your cluster, such as the Elastic Operator and the Dracon Helm package. Depending on the capabilities of your workstation this will probably
+take a couple of minutes. It's perfect time to go get a cup of coffee! 😉
+
+```text
+   )  (
+  (   ) )
+   ) ( (
+  -------
+.-\     /
+'- \   /
+  _______
+```
+
+
+
+### 3. Deploy Dracon Components
+
+Next, we need to install some components. Our components can be easily installed
+as follows:
+
+```bash
+helm upgrade \
+  --install \
+  --namespace dracon \
+  --version 0.15.0 \
+  dracon-oss-components \
+  oci://ghcr.io/ocurity/dracon/charts/dracon-oss-components
+```
+
+And that's it! Dracon is now up and running on your cluster and you can start using it.
+
+# Running a Pipeline
+
+That's what we're actually here for. Let's run a pipeline. We can do this by
+using one of the example pipelines included in the repository.
+
+Let's choose the Go pipeline as an example. You can deploy the pipeline as follows:
+
+```bash
+make cmd/draconctl/bin
+bin/cmd/draconctl pipelines deploy ./examples/pipelines/python-project
+```
+
+This deploys the pipeline to Dracon. You can see that the pipeline was created
+by taking a look at the Tekton dashboard. You can access the dashboard by running:
+
+```bash
+kubectl -n tekton-pipelines port-forward svc/tekton-dashboard 9097:9097
+# Then open your browser at http://localhost:9097
+```
+
+You should see the pipeline in the dashboard:
+![Screenshot of the Tekton Dashboard showing the Dracon Go Pipeline](/img/quickstart-pipeline.png)
+
+Then you can run an instance of the pipeline as follows:
+
+```bash
+kubectl create \
+  -n dracon \
+  -f ./examples/pipelines/python-project/pipelinerun.yaml
+```
+
+And that's it! You've just run your first pipeline with Dracon. 🎉
+To see the pipeline running, head to `PipelineRuns` in the Tekton dashboard.
---
+++ docusaurus.config.ts
@@ -0,0 +1,122 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Smithy Docs',
+  tagline: 'Focus on the important stuff.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://docs.smithy.security',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ocurity', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ocurity/docs/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Smithy Docs',
+      logo: {
+        alt: 'Smithy Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/ocurity/docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/quickstart',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/ocurity/docs',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} SPF Security Solutions Ltd. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
