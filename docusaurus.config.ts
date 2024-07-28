cb35a54835d867da7e47d3f75045d22d13b46b2a
Flo Wirtz <6052785+flowirtz@users.noreply.github.com>
2024-07-28 11:41:44 +0300

    💄 Update footer colour

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,24 +7,17 @@ const config: Config = {
   tagline: "Focus on the important stuff.",
   favicon: "img/favicon.png",
 
-  // Set the production url of your site here
   url: "https://docs.smithy.security",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ocurity", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  organizationName: "ocurity",
+  projectName: "docs",
   trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -36,8 +29,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/ocurity/docs/tree/main/",
         },
         theme: {
@@ -70,7 +61,6 @@ const config: Config = {
       ],
     },
     footer: {
-      style: "dark",
       links: [
         {
           title: "Docs",
@@ -86,7 +76,7 @@ const config: Config = {
           items: [
             {
               label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              href: "https://stackoverflow.com/questions/tagged/smithy",
             },
             {
               label: "Discord",
