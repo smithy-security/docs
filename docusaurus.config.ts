b32adfeba48cd6c70a7dd0832bd5f4331be01d18
Flo Wirtz <6052785+flowirtz@users.noreply.github.com>
2024-07-28 11:28:00 +0300

    🍱 Change social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,8 +48,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/social-card.png",
     navbar: {
       title: "Docs",
       logo: {
